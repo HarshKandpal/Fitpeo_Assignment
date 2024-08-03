3ff9c53653d9e6edab90076194b94a1a95339f07
HarshKandpal <harshkandpal172000@gmail.com>
2024-08-03 23:16:48 +0530

    Fixes 2.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,3 @@
-// import React from 'react';
-// import Home from './Components/Home';
-// import Navbar from './Components/Navbar';
-// import Sidebar from './Components/Sidebar';
-
-// function App() {
-//   return (
-//     <div className="bg-primary min-h-screen flex flex-col overflow-x-hidden">
-//       <div className="grid grid-cols-[auto,1fr] flex-grow">
-//         <Sidebar />
-//         <div className="flex flex-col flex-grow">
-//           <Navbar />
-//           <main className="flex-1 p-4 overflow-auto">
-//             <Home />
-//           </main>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
 import React from 'react';
 import Home from './Components/Home';
 import Navbar from './Components/Navbar';
